0b6f183d972187672aee8ef293fd04639fba3e5e
mucrow <120820315+mucrow@users.noreply.github.com>
2023-02-04 12:16:34 -0500

    comment the PlayerController code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,40 +21,65 @@ public class PlayerController: MonoBehaviour {
   Vector2 _velocity = Vector2.zero;
 
   void Update() {
+    // Left Arrow, Right Arrow, the A key, or the D key. works for gamepads too
     var runInput = Input.GetAxis("Horizontal");
+
+    // spacebar
     var isJumpPressed = Input.GetButton("Jump");
+
+    // this is left mouse button. i call the action "build" ("Fire1" is the name of the left mouse
+    // button input in Unity default input mapping)
     var isBuildPressed = Input.GetButtonDown("Fire1");
     var onGround = _onGroundTrigger.IsEntered();
 
+    // 0.1 is the analog stick deadzone for running
     if (Math.Abs(runInput) > 0.1) {
+      // run if there's an input
       _velocity.x = runInput * RunSpeed;
     }
     else {
+      // stop if there isn't
       _velocity.x = 0;
     }
 
+    // if the player is on the ground...
     if (onGround) {
+      // and the jump button is pressed, and they are not rising...
       if (isJumpPressed && _velocity.y <= 0) {
+        // make them jump
         _velocity.y = JumpForce;
       }
       else {
+        // set velocity to 0 if the player is on the ground but not trying to jump
         _velocity.y = 0;
       }
     }
     else {
+      // if the player is not on the ground, apply gravity.
+      // this is scaled with deltaTime because the player should accelerate downward
       _velocity.y -= 9.87f * Time.deltaTime;
     }
 
+    // apply the velocity to the rigidbody
+    // TODO check if we actually need the _velocity variable (maybe we can use _rigidbody2D.velocity directly)
     _rigidbody2D.velocity = _velocity;
 
+    // if the player pressed the build button...
     if (isBuildPressed) {
+      // this is debugging code. i don't know how to access tile data correctly (so the player
+      // knows what tile theyre placing) so i just find a non-empty tile in the level and use that
       var tileData = FindATile();
-      // var pos = FindAnEmptyTile();
+
+      // use the player's position to determine the target position of the tile we're placing
+      // basically it's (player.x, player.y - 1)
       var pos = new Vector3Int(Mathf.FloorToInt(transform.position.x), Mathf.FloorToInt(transform.position.y - 1), 0);
+
+      // modify the world
       _tilemap.SetTile(pos, tileData);
     }
   }
 
+  // returns a non-empty tile in the range of (-10, -10) to (10, 10) in the world
   TileBase FindATile() {
     for (int y = -10; y < 10; ++y) {
       for (int x = -10; x < 10; ++x) {
@@ -66,18 +91,5 @@ public class PlayerController: MonoBehaviour {
       }
     }
     return null;
-  }
-
-  Vector3Int FindAnEmptyTile() {
-    for (int y = -10; y < 10; ++y) {
-      for (int x = -10; x < 10; ++x) {
-        var pos = new Vector3Int(x, y, 0);
-        var tileData = _tilemap.GetTile(pos);
-        if (tileData == null) {
-          return pos;
-        }
-      }
-    }
-    return new Vector3Int(0, 0, 0);
   }
 }
